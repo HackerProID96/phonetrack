0615c6dc6a5e21e38ed45c619077d2d77edf6d95
HackerProID96 <91298985+HackerProID96@users.noreply.github.com>
2021-09-24 16:33:09 +0700

    Update PhoneTrack.py

--- PhoneTrack.py
+++ PhoneTrack.py
@@ -17,3 +17,10 @@ print (geocoder.description_for_number(ch_nmber, "en"))
 from phonenumbers import carrier
 service_nmber = phonenumbers.parse(number, "RO")
 print(carrier.name_for_number(service_nmber, "en"))
+
+except KeyboardInterrupt:
+        print ('Terminating, Bye'+lgreen)
+        sys.exit(0)
+except requests.exceptions.ConnectionError as e:
+        print (red+"[~]"+" check your internet connection!"+clear)
+sys.exit(1)
