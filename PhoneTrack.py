ca7fba8c05aa77b0a33b2d324e712d52f94d5f86
HackerProID96 <91298985+HackerProID96@users.noreply.github.com>
2021-09-24 16:25:32 +0700

    Update PhoneTrack.py

--- PhoneTrack.py
+++ PhoneTrack.py
@@ -5,7 +5,7 @@ from sys import argv
 from phonenumbers import geocoder
 
 parser = argparse.ArgumentParser()
-parser.add_argument ("-v", help= "target/host IP address", type=str, dest='target', required=True )
+parser.add_argument ("-v", help= "Phone Number", type=str, dest='target', required=True )
 
 args = parser.parse_args()
 
