52ee9a259e68210933aaa00a391e8da95a4c8b83
HackerProID96 <91298985+HackerProID96@users.noreply.github.com>
2021-09-24 16:08:44 +0700

    Update PhoneTrack.py

--- PhoneTrack.py
+++ PhoneTrack.py
@@ -3,14 +3,10 @@ import phonenumbers
 from phonenumbers import geocoder
 
 number = args.target
-
 ch_nmber = phonenumbers.parse(number, "CH")
-
 print (geocoder.description_for_number(ch_nmber, "en"))
 
 
 from phonenumbers import carrier
-
 service_nmber = phonenumbers.parse(number, "RO")
-
 print(carrier.name_for_number(service_nmber, "en"))
