b6d64382b7c8aa84a2a48faedb01d2889d55432e
HackerProID96 <91298985+HackerProID96@users.noreply.github.com>
2021-09-24 19:10:40 +0700

    Update PhoneTrack.py

--- PhoneTrack.py
+++ PhoneTrack.py
@@ -3,6 +3,8 @@ import argparse
 import sys
 from sys import argv
 from phonenumbers import geocoder
+from phonenumbers import carrier
+from phonenumbers import timezone
 
 parser = argparse.ArgumentParser()
 parser.add_argument ("-v", help= "Phone Number", type=str, dest='target', required=True )
@@ -10,18 +12,27 @@ parser.add_argument ("-v", help= "Phone Number", type=str, dest='target', requir
 args = parser.parse_args()
 
 number = args.target
-ch_nmber = phonenumbers.parse(number, "CH")
-print("Internasional Format: ", phonenumbers.format_number(
-            PhoneNumberObject, phonenumbers.PhoneNumberFormat.INTERNATIONAL).split(' ')[0])
-print("Local Format: ", phonenumbers.region_code_for_country_code(
-            int(numberCountryCode))
-print("Country Found: ", geocoder.country_name_for_number(PhoneNumberObject, "en"))
-print ("City/Area: ", geocoder.description_for_number(ch_nmber, "en"))
-
-
-from phonenumbers import carrier
 service_nmber = phonenumbers.parse(number, "RO")
-print(carrier.name_for_number(service_nmber, "en"))
+ch_nmber = phonenumbers.parse(number, "CH")
+            
+print(red+"-------------------[PHONE NUMBER TRACKING]-------------------"+red)
+print(green+"[Internasional Format]: "+green, phonenumbers.format_number(
+            PhoneNumberObject, phonenumbers.PhoneNumberFormat.INTERNATIONAL).split(' ')[0])
+print(green+[Local Format]: "+green, phonenumbers.region_code_for_country_code(
+            int(numberCountryCode))
+print(green+"[Country Found]: "+green, geocoder.country_name_for_number(PhoneNumberObject, "en"))
+print (green+"[City/Area]: "+green, geocoder.description_for_number(ch_nmber, "en"))
+print(green+"[Carrier]: "+green, carrier.name_for_number(service_nmber, "en"))
+print(green+"[Timezone]: "+green, timezone.time_zones_for_number(PhoneNumberObject):
+)
+
+
+
+if phonenumbers.is_possible_number(PhoneNumberObject):
+            print('The number is valid and possible.')
+        else:
+            print('The number is valid but might not be possible.')
+
 
 except KeyboardInterrupt:
         print ('Terminating, Bye'+lgreen)
